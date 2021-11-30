a2fe5fabf4cce6c161082f8933b96a8c95069c82
Mansanarez Valentin <valentin.mansanarez@irstea.fr>
2021-11-30 10:27:50 +0100

    [DATASET]: ass of two dataset and their descriptions to be able to have reproductible examples.

---
+++ R/StationQ_TS-data.R
@@ -0,0 +1,26 @@
+#' Few Discharge time series from the French national discharge database "Banque HYDRO"
+#'
+#' Data from a QTL experiment on gravitropism in
+#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
+#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
+#' increments over eight hours.
+#'
+#' @docType data
+#'
+#' @usage data(StationQ_3catch)
+#'
+#' @format A list of two tibble objects: \code{"data"} and \code{"info}.
+#'
+#' @keywords datasets
+#'
+#' @references Moore et al. (2013) Genetics 195:1077-1086
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#'
+#' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
+#'
+#' @examples
+#' data(grav)
+#' times <- attr(grav, "time")
+#' phe <- grav$pheno
+#' \donttest{iplotCurves(phe, times)}
+"StationQ_TS"
