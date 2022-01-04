cb026c4587f538a7baccf3acf541d04a344d90e5
Mansanarez Valentin <valentin.mansanarez@irstea.fr>
2022-01-04 15:20:14 +0100

    [DATA]: Sample dataset added for illustrating and documenting the functions with reproductible examples.

---
+++ R/StationQ_3catch.R
@@ -0,0 +1,13 @@
+#' Few Discharge time series from the French national discharge database "Banque HYDRO"
+#'
+#' Data from a QTL experiment on gravitropism in
+#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
+#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
+#' increments over eight hours.
+#'
+#' @format A list of two tibble objects:
+#' \describe{
+#'  \item{data}{Date to analyse: different classifications/groups and corresponding value}
+#'  \item{info}{Info corresponding to each group}
+#' }
+"StationQ_3catch"
